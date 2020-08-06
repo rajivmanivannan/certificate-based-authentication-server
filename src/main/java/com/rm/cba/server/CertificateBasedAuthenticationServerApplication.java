0c0ded80a784206a3ba77e59baf12f7a1f9091dc
rajivmanivannan <rajivroczzz@gmail.com>
2020-08-07 02:22:22 +0530

    Initial commit

---
+++ src/main/java/com/rm/cba/server/CertificateBasedAuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.rm.cba.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateBasedAuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateBasedAuthenticationServerApplication.class, args);
+	}
+
+}
